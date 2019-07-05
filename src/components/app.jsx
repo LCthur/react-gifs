63de4435dfecf57bd5d118e7cc310f9cec9bee73
Loic Thurre <loic.thurre@gmail.com>
2019-07-05 12:19:46 +0200

    giflist implemented

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 
 import SearchBar from './searchBar';
+import Gif from './gif';
+import GifList from './gifList';
 
 
 class App extends Component {
   render() {
+    const gifs = [
+      { id: 'j38LTRE3LiirEaFLz9' },
+      { id: '3o6Zt6NPQItgZEK10c' }
+    ];
+
+
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
+          <div className="selected-gif">
+            <Gif id="JUeBAaFX0fBfycynYb" />
+          </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs={gifs} />
+        </div>
       </div>
     );
   }
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,1 +1,12 @@
-gif.jsx
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  render() {
+    const src = `https://media.giphy.com/media/${this.props.id}/200.gif`;
+    return (
+      <img src={src} alt="a gif" className="gif" />
+    );
+  }
+}
+
+export default Gif;
